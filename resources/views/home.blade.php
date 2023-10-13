341fcdcf50bb480471d7ac01b663196f2daebdcd
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-10-13 15:43:57 +0200

    add print all info cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,21 @@
 
 @section('main-content')
   <div class="container py-2">
-    <div class="row  align-items-center g-2">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
       @foreach ($movies as $movie)
-      <div class="col-3">
-          <div class="card h-100 mb-2">
-            <div class="card-media">
-            </div>
-            <div class="card-text">  
-              {{$movie["title" ]}}                          
-            </div>
-          </div> 
+      <div class="col">
+        <div class="card h-100">
+          {{-- <img src="..." class="card-img-top" alt="..."> --}}
+          <div class="card-body">
+            <h5 class="card-title">{{$movie["title"]}}</h5>
+            <ul class="card-text">
+              <li>{{$movie["original_title"]}}</li>
+              <li>{{$movie["nationality"]}}</li>
+              <li>{{$movie["date"]}}</li>
+              <li>{{$movie["vote"]}}</li>
+            </ul>
+          </div>
+        </div> 
         </a>
       </div>
       @endforeach
